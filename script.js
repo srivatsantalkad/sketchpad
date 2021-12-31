1a131388b2e51d7cb157197b5069dbc3c6ad9d4b
Srivatsan Talkad <stalkad01@gmail.com>
2021-12-30 20:52:46 -0500

    Create 2 container divs in index.html & createGrid function in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// This function manipulates the DOM to create a grid of divs that is gridLength x gridWidth.
+function createGrid(gridLength, gridWidth) {
+
+}
+
+const GRID_LENGTH = 16;
+const GRID_WIDTH = 16;
+
+createGrid(GRID_LENGTH, GRID_WIDTH);
